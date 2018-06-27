2ac404448a9b71a6f46ece4f429da62edd286526
sounak <sounak@sounak-Lenovo-G570>
2018-06-27 08:46:24 +0530

    Spring boot test project

---
+++ workspace-sts-3.9.4.RELEASE/RestEndpointGit/src/main/java/com/gitdeploy/test/RestEndpointGitApplication.java
@@ -0,0 +1,12 @@
+package com.gitdeploy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEndpointGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEndpointGitApplication.class, args);
+	}
+}
